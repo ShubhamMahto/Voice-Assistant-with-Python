f9cf58292c74a6f196aa0b1444aef97bce60d3b2
ShubhamMahto <shubhamsama19iteng@student.mes.ac.in>
2022-01-13 14:11:52 +0530

    Main Python file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyttsx3 as p
 
 engine = p.init()
-engine.say("What Can i do for you Sir?")
+engine.setProperty('rate', 175)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say("Hello There, I am your Voice Assistant.")
 engine.runAndWait()
